6e231dedf4ff86f27b037fcbe1fe135c23447645
lufebraz <luizbraz98@gmail.com>
2023-04-10 16:43:43 -0300

    adicionando gitlab prints

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "github_repository" "lufebraz" {
-  name        = "projeto-final-int-cnt-devops"
+  name        = "_projeto-final-int-cnt-devops"
   description = "devops deploy"
 
   visibility = "public"
